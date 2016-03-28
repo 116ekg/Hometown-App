da3a790c4a5e3d95fbb0e8eff3c899256f024e46
eddie <116ekg@gmail.com>
2016-03-27 21:56:29 -0700

    Initial Commit

---
+++ favorite-place/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  favorite-place
+//
+//  Created by Eddie Keller on 3/27/16.
+//  Copyright © 2016 Eddie Keller. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
